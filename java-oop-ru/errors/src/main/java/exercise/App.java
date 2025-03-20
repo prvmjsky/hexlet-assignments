41ba5b0931fbb61a6776da52605df866fb3a9566
prvmjsky <lukyanovalexey1@gmail.com>
2025-03-20 17:41:50 +0500

    create App and Circle

--- java-oop-ru/errors/src/main/java/exercise/App.java
+++ java-oop-ru/errors/src/main/java/exercise/App.java
@@ -1,5 +1,16 @@
 package exercise;
 
 // BEGIN
-
+public class App {
+    public static void printSquare(Circle circle) {
+        try {
+            var roundedSquare = Math.round(circle.getSquare());
+            System.out.print(roundedSquare + "\n");
+        } catch (NegativeRadiusException e) {
+            System.out.print(e.getMessage() + "\n");
+        } finally {
+            System.out.print("Вычисление окончено" + "\n");
+        }
+    }
+}
 // END
--- java-oop-ru/errors/src/main/java/exercise/Circle.java
+++ java-oop-ru/errors/src/main/java/exercise/Circle.java
@@ -1,5 +1,23 @@
 package exercise;
 
-// BEGIN
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+// BEGIN
+@AllArgsConstructor
+@Getter
+@Setter
+public class Circle {
+    private Point point;
+    private int radius;
+
+    public double getSquare() throws NegativeRadiusException {
+        if (radius < 0) {
+            throw new NegativeRadiusException("Не удалось посчитать площадь");
+        }
+
+        return Math.PI * (radius * radius);
+    }
+}
 // END

d2b311b4f5802990697a47ca0fcb5e91b5dacbec
@hexlet/cli <support@hexlet.io>
2025-03-21 15:45:30 +0500

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Address.java
+++ java-oop-ru/reflections/src/main/java/exercise/Address.java
@@ -1,33 +1,33 @@
 package exercise;
 
 class Address {
-    // BEGIN
-    
-    // END
-    private String country;
+	// BEGIN
+	@NotNull
+	// END
+	private String country;
 
-    // BEGIN
-    
-    // END
-    private String city;
+	// BEGIN
+	@NotNull
+	// END
+	private String city;
 
-    // BEGIN
-    
-    // END
-    private String street;
+	// BEGIN
+	@NotNull
+	// END
+	private String street;
 
-    // BEGIN
-    
-    // END
-    private String houseNumber;
+	// BEGIN
+	@NotNull
+	// END
+	private String houseNumber;
 
-    private String flatNumber;
+	private String flatNumber;
 
-    Address(String country, String city, String street, String houseNumber, String flatNumber) {
-        this.country = country;
-        this.city = city;
-        this.street = street;
-        this.houseNumber = houseNumber;
-        this.flatNumber = flatNumber;
-    }
+	Address(String country, String city, String street, String houseNumber, String flatNumber) {
+		this.country = country;
+		this.city = city;
+		this.street = street;
+		this.houseNumber = houseNumber;
+		this.flatNumber = flatNumber;
+	}
 }
--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -1,6 +1,27 @@
 package exercise;
 
 import java.lang.reflect.Field;
-// BEGIN
+import java.util.Arrays;
+import java.util.List;
 
+// BEGIN
+public class Validator {
+	public static List<String> validate(Object object) {
+		var aClass = object.getClass();
+		var fields = aClass.getDeclaredFields();
+
+		return Arrays.stream(fields)
+			.filter(field -> field.isAnnotationPresent(NotNull.class))
+			.filter(field -> {
+				try {
+					field.setAccessible(true);
+					return field.get(object) == null;
+				} catch (IllegalAccessException e) {
+					throw new RuntimeException(e);
+				}
+			})
+			.map(Field::getName)
+			.toList();
+	}
+}
 // END
